d53afd1950fbdc75bdfd90df873e6ffeff4446f6
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-05-20 10:49:19 +0200

    changement de version dans le fichier pom.xml

---
+++ src/main/java/com/example/heartbeast/HeartbeastApplication.java
@@ -0,0 +1,13 @@
+package com.example.heartbeast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartbeastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartbeastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/heartbeast/HeartbeastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heartbeast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartbeastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
