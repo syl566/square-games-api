f0b76983d8eb067fef51712de5e0047897ea4335
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-06-04 13:41:53 +0200

    version

--- src/main/java/com/example/heartbeat/entity/GameEntityRepository.java
+++ src/main/java/com/example/heartbeat/entity/GameEntityRepository.java
@@ -2,6 +2,7 @@ package com.example.heartbeat.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameEntityRepository extends JpaRepository<GameEntity, String> {
+
+public interface GameEntityRepository extends JpaRepository<GameEntity,String> {
 
 }
